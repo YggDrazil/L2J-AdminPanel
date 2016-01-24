6c98396e2d64ea8100f44510a8d8b253a4f351ea
George Tzinos <geotzinos@gmail.com>
2016-01-24 08:18:48 +0200

    New app new commit

---
+++ L2J_Admin_Panel/Form1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace L2J_Admin_Panel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
